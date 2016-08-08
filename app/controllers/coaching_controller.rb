dcd3c4fce76cdf12add15d229f4e4106f2e48551
Alex <alexduval71@gmail.com>
2016-08-08 12:30:53 +0200

    inplemant the previous exo from day 1

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,14 +1,35 @@
 class CoachingController < ApplicationController
   def home
-
   end
 
   def ask
-
   end
 
   def answer
     @query = params[:query]
-    @debile = ["peut être","Mister Brown", "Effectivement, tu as raison", "Je crois que c'est bleu", "Mais oui, c'est clair", "Kakamoulox"]
+    @anwser = coach_answer_enhanced(@query)
+#    @debile = ["peut être","Mister Brown", "Effectivement, tu as raison", "Je crois que c'est bleu", "Mais oui, c'est clair", "Kakamoulox"]
+
+  end
+
+end
+
+private
+
+def coach_answer(your_message)
+  if your_message.slice(your_message.length - 1) == "?"
+    return "Silly question, get dressed and go to work!"
+  elsif your_message == "I am going to work right now!"
+    return "C'est bien"
+  else
+    return "I don't care, get dressed and go to work!"
+  end
+end
+
+def coach_answer_enhanced(your_message)
+  if your_message == "I am going to work right now!".upcase
+    return ""
+  else
+    return "I can feel your motivation! #{coach_answer(your_message)}"
   end
 end
